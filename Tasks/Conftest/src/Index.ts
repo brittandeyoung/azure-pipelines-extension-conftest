38a33817009829976a29d0cb367f07142fba4fb3
Brittan DeYoung <bdeyoung@hagerty.com>
2020-06-10 14:27:19 -0400

    testing new arg building method

--- Tasks/Conftest/src/Index.ts
+++ Tasks/Conftest/src/Index.ts
@@ -6,26 +6,32 @@ import path = require('path');
 
 async function runConftest() {
     let inputCommand = tasks.getInput("conftestCommand", true);
-    let inputArgs = tasks.getInput("conftestArgs", false) ?? "";
-    let inputFile = tasks.getInput("conftestFile", false) ?? ""; 
+    let inputArgs = tasks.getInput("conftestArgs", false);
+    let inputFile = tasks.getInput("conftestFile", false); 
     let envPath = process.env['PATH'];
     let conftestPath = tasks.which("conftest", true);
     let conftestTool : ToolRunner = tasks.tool(conftestPath);
 
-    if (inputCommand == "test") {
-      conftestTool.arg([inputCommand!, inputFile!, inputArgs!]);
-      return conftestTool.exec();
-    }
-    
-    if (inputCommand == "parse") {
-      conftestTool.arg([inputCommand!, inputFile!, inputArgs!]);
-      return conftestTool.exec();
-    }
+    conftestTool.argIf(inputCommand != undefined, inputCommand)
+    conftestTool.argIf(inputArgs != undefined, inputArgs)
+    conftestTool.argIf(inputFile != undefined, inputFile)
 
-    else {
-      conftestTool.arg([inputCommand!, inputArgs!]);
-      return conftestTool.exec();
-    }  
+    return conftestTool.exec();
+
+    // if (inputCommand == "test") {
+    //   conftestTool.arg([inputCommand!, inputFile!, inputArgs!]);
+    //   return conftestTool.exec();
+    // }
+    // conftestTool.argIf(inputFile != undefined, inputFile!)
+    // if (inputCommand == "parse") {
+    //   conftestTool.arg([inputCommand!, inputFile!, inputArgs!]);
+    //   return conftestTool.exec();
+    // }
+
+    // else {
+    //   conftestTool.arg([inputCommand!, inputArgs!]);
+    //   return conftestTool.exec();
+    // }  
 }
 
 async function run() {
