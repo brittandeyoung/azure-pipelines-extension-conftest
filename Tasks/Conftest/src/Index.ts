c89a241b2fb2b73ed334ca74e2f57de545cda7fe
Brittan DeYoung <bdeyoung@hagerty.com>
2020-07-01 09:07:33 -0400

    added more options to support all comands

--- Tasks/Conftest/src/Index.ts
+++ Tasks/Conftest/src/Index.ts
@@ -8,13 +8,29 @@ async function runConftest() {
     let inputCommand = tasks.getInput("command", true);
     let inputArgs = tasks.getInput("arguments", false);
     let inputFile = tasks.getInput("file", false); 
+    let inputFilePath = tasks.getInput("filepath", false); 
+    let inputRepo = tasks.getInput("repository", false); 
     let envPath = process.env['PATH'];
     let conftestPath = tasks.which("conftest", true);
     let conftestTool : ToolRunner = tasks.tool(conftestPath);
 
-    conftestTool.argIf(inputCommand != undefined, inputCommand)
-    conftestTool.argIf(inputFile != undefined, inputFile)
-    conftestTool.argIf(inputArgs != undefined, inputArgs)
+    if (inputCommand == "test" || inputCommand == "parse") {
+        conftestTool.argIf(inputCommand != undefined, inputCommand)
+        conftestTool.argIf(inputFile != undefined, inputFile)
+        conftestTool.argIf(inputArgs != undefined, inputArgs)
+    }
+    
+    if (inputCommand == "verify") {
+      conftestTool.argIf(inputCommand != undefined, inputCommand)
+      conftestTool.argIf(inputArgs != undefined, inputArgs)
+    }
+
+    if (inputCommand == "pull" || inputCommand == "push") {
+        conftestTool.argIf(inputCommand != undefined, inputCommand)
+        conftestTool.argIf(inputRepo != undefined, inputRepo)
+        conftestTool.argIf(inputFilePath != undefined, inputFilePath)
+        conftestTool.argIf(inputArgs != undefined, inputArgs)
+    }
 
     return conftestTool.exec();
 }
